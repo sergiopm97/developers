c3cad12c958de37f3439fcb13ee1d1bd725a278e
Sergio Peña Muñoz <92332731+sergiopm97@users.noreply.github.com>
2022-02-12 12:26:35 +0100

    Create user interface structure
    Implementation of the folder and file structure of the user interface - Creation of the general server path dedicated to a user's registration

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ require("dotenv").config();
 const server = express();
 const PORT = process.env.PORT || 80;
 
+server.set("view engine", "ejs");
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static("public"));
+
+server.use("/", (req, res)  => {
+    res.render("index");
+});
+
 server.listen(PORT, () => {
     console.log(`\n-> Server listening on port ${PORT}`);
 });
