fa8cd8be33feb2b4540557df8acf40e73e25a2b0
Sergio Peña Muñoz <92332731+sergiopm97@users.noreply.github.com>
2022-02-13 12:08:58 +0100

    Create developer model
    Creation of the schema and the model of the developer

---
+++ models/Developer.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const developerSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+}, {
+    timestamps: true
+});
+
+const Developer = mongoose.model("Developer", developerSchema);
+
+module.exports = Developer;
